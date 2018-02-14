d66a69554040a6b5fa865012f62fee75230874ac
GlennCarroy <g_carroy@live.be>
2018-02-14 16:59:17 +0100

    list OK sans les bonus

--- contenu.php
+++ contenu.php
@@ -1,70 +1,33 @@
-<!-- contenu afficher -->
-<?php 
-$json_file = file_get_contents('todo.json'); //on load le contenu du JSON
-$data = json_decode($json_file, true); // on décode le contenu du JSON
-?>
-
+<!-- Section à afficher dans l'HTML -->
 <section>
-    <h1>A faire</h1>
-    <form method="post">
-    		<ul>
-				<?php 
-
-					if (isset($data["todo"])) {
-						foreach ($data["todo"] as $key => $value) {
-							//On affiche chacune des taches présente dans le fichier JSON
-							?>
-							<li><input type="checkbox" name="case[]" value="<?=$value?>"><label for="case"><?=$value?></label></li>
-							<?php
-						}
-					}
-				?>
-			</ul>
+  <h1>A faire</h1>
+ 	<form method="post">
+    <ul>
+		<?php 
+			if (isset($data["todo"])) {
+				foreach ($data["todo"] as $key => $value) {
+					//On affiche chacune des taches présente dans le tableau todo du fichier JSON
+					?>
+					<li><input type="checkbox" name="case[]" value="<?=$value?>"><label for="case"><?=$value?></label></li>
+					<?php
+				}
+			}
+		?>
+		</ul>
 		<input type="submit" value="Archiver">
 	</form>
 </section>
 <section>
-    <h1>Archive</h1>
-
-<?php 
-/* ON ARCHIVE LES DONNES */
-
-	/*On encode les taches check dans le tableau archives du fichier JSON*/
-	if (isset($_POST['case'])){
-		$archives = $_POST['case'];
-
-		foreach ($archives as $key => $value) {
-			$data["archives"][] = $value;
-
-						// foreach ($data["todo"] as $key => $value) {
-						// 	if ($value !== $data["archives"][$key]) {
-						// 		///////////
-						// 	}
-						// }
-
-					// }
-
-
-		};  // on ajoute la tache dans le tableau data
-		$A = array_diff( $data["todo"] , $archives);			
-		var_dump($archives);
-		var_dump($data["todo"]);
-		var_dump($A);
-		$ecriture_tache = json_encode($data, JSON_FORCE_OBJECT); //on encode la tache en format JSON
-		$stock_tache = file_put_contents('todo.json' , $ecriture_tache); //rajouter dans le fichier JSON
-
-	};
-
-	/* ON AFFICHE LES ARCHIVES */
-		
-
-		if (isset($data["archives"])) {
-			foreach ($data["archives"] as $key => $value) {
-				//On affiche chacune des taches présente dans le fichier JSON
-				?>
-				<p style="text-decoration: line-through; color: grey;"><?=$value?></p>
-				<?php
+  <h1>Archive</h1>
+	<?php 
+		/* ON AFFICHE LES ARCHIVES */
+			if (isset($data["archives"])) {
+				foreach ($data["archives"] as $key => $value) {
+					//On affiche chacune des taches présente dans le tableau archive du fichier JSON
+					?>
+					<p style="text-decoration: line-through; color: grey;"><?=$value?></p>
+					<?php
+				}
 			}
-		}
- ?> 
+	 ?> 
 </section>
--- formulaire.php
+++ formulaire.php
@@ -1,30 +1,47 @@
-<!-- L'ajout de la tache -->
 <?php 
+//on load le contenu du JSON
+$json_file = file_get_contents('todo.json');
+// on décode le contenu du JSON
+$data = json_decode($json_file, true); 
 
-/*On encode les taches dans le tableau todo du fichier JSON*/
-if(isset($_POST["tache"])) {
-	$tache = htmlspecialchars($_POST["tache"]);
-	$json_file = file_get_contents('todo.json'); //on load le contenu du JSON
-	$data = json_decode($json_file, true); // on décode le contenu du JSON
-	$data["todo"][] = $tache; // on ajoute la tache dans le tableau data
-	$ecriture_tache = json_encode($data, JSON_FORCE_OBJECT); //on encode la tache en format JSON
-	$stock_tache = file_put_contents('todo.json' , $ecriture_tache); //rajouter dans le fichier JSON
-}
+/*ENCODAGE DES TACHES EN JSON*/
+	if(isset($_POST["tache"])) {
+		$tache = htmlspecialchars($_POST["tache"]);
+	// on ajoute la tache dans le tableau $data
+		$data["todo"][] = $tache;
+	//Le tableau retourne au format JSON
+		$encodage_tableau = json_encode($data, JSON_FORCE_OBJECT);
+	//On ajout le tableau dans le fichier JSON
+		file_put_contents('todo.json' , $encodage_tableau);
+		}
 
+/* ARCHIVAGE DES TACHES */
+	/*On encode les taches "check" dans le tableau archives du fichier JSON*/
+	if (isset($_POST['case'])){
+		$archives = $_POST['case'];
+	// on ajoute les taches cochées dans le tableau archives de $data
+		foreach ($archives as $key => $value) {
+			$data["archives"][] = $value;
+		}
+	/* On réécrit le tableau todo du fichier JSON */
+	//On écrase le premier tableau en comparant $archives avec l'ancien "todo"
+		$data["todo"] = array_diff( $data["todo"] , $archives);			
+		$encodage_tableau = json_encode($data, JSON_FORCE_OBJECT);
+		file_put_contents('todo.json' , $encodage_tableau); 
+	}
 ?>
 <!DOCTYPE html>
 <html>
   <head>
-  	<title>TP</title>
+  	<title>To do list</title>
 	<meta charset="UTF-8">
   </head>
 <body>
     <h1>ToDoList</h1>
-	<p>To do List</p>
-	<form method="POST">
-		<input type="text" name="tache" placeholder="What do you want to do?" maxlength="50" required>
-		<input type="submit">
-	</form>
+		<form method="POST">
+			<input type="text" name="tache" placeholder="What do you want to do?" maxlength="50" required>
+			<input type="submit">
+		</form>
 
 <?php include 'contenu.php'; ?>
 
